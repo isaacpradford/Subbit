ebfb2b37ec4120dc972ebd32648bff704186ca10
Isaac Radford <radf.isaac@gmail.com>
2024-03-15 13:54:22 -0600

    Finished user registration form

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 // Server setup
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+
 app.use(express.json())
+   .use(cors())
    .use(require('./routes'));
 
 app.listen(5500, () => console.log('Server running on port 5500'));
