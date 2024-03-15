fb44de12bb3347917ddeaa1fccab9c0eb9858b19
Isaac Radford <radf.isaac@gmail.com>
2024-03-15 14:09:50 -0600

    Finished adding frontend tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import UserRegistration from './components/UserRegistration';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+//Ensures form is rendered
+test('renders registration form', () => {
+  render(<UserRegistration />);
+  const nameLabel = screen.getByText(/Full Name/i);
+  const emailLabel = screen.getByText(/Email/i);
+  const passwordLabel = screen.getByText(/Password/i);
+  const submitButton = screen.getByText(/Submit/i);
+
+  expect(nameLabel).toBeInTheDocument();
+  expect(emailLabel).toBeInTheDocument();
+  expect(passwordLabel).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+
+// Ensures form is usable
+test('form can be filled and submitted', async () => {
+  render(<UserRegistration />);
+  const nameInput = screen.getByPlaceholderText(/Full Name/i);
+  const emailInput = screen.getByPlaceholderText(/Email/i);
+  const passwordInput = screen.getByPlaceholderText(/Password/i);
+  const submitButton = screen.getByText(/Submit/i);
+
+  fireEvent.change(nameInput, { target: { value: 'test' } });
+  fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+  fireEvent.change(passwordInput, { target: { value: 'test123' } });
+
+  fireEvent.click(submitButton);
 });
